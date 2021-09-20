d139a3a3be841b442f728ee715ebe6723c41b377
Venura <valw0002@student.monash.edu>
2021-09-20 20:43:35 +1000

    Adding Backward Elimination to remove unpredictive variables

--- regression/multiple_linear_regression.py
+++ regression/multiple_linear_regression.py
@@ -26,5 +26,32 @@ score = regressor.score(x_train, y_train)
 # Predicting a new value
 y_pred = regressor.predict([[0, 1, 0, 130000, 140000, 300000]])
 
+# Backward Elimination
+# Remove a dummy variable column and adding the constant column
+x = x[:, 1:]
+x = np.append(np.ones((len(x), 1), dtype=int), x, axis=1)
+
+print(x)
+
+import statsmodels.api as sm
+x_regressor = np.array(x, dtype=float)
+ordinary_least_squares_regressor = sm.OLS(endog= y, exog= x_regressor).fit()
+
+# Removing New York
+x_regressor = np.array(x[:, [0, 1, 3, 4, 5]], dtype=float)
+ordinary_least_squares_regressor = sm.OLS(endog= y, exog= x_regressor).fit()
+
+# Removing Florida
+x_regressor = np.array(x[:, [0, 3, 4, 5]], dtype=float)
+ordinary_least_squares_regressor = sm.OLS(endog= y, exog= x_regressor).fit()
+
+# Removing Administration
+x_regressor = np.array(x[:, [0, 3, 5]], dtype=float)
+ordinary_least_squares_regressor = sm.OLS(endog= y, exog= x_regressor).fit()
+
+# Removing Marketing Spend
+x_regressor = np.array(x[:, [0, 3]], dtype=float)
+ordinary_least_squares_regressor = sm.OLS(endog= y, exog= x_regressor).fit()
+print(ordinary_least_squares_regressor.summary())
 
 
