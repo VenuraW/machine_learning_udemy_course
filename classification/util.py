53ac74c564e0ce0843c5e9c4d6c8c517f5b212da
Venura <valw0002@student.monash.edu>
2021-09-29 14:09:20 +1000

    Refactoring return values

--- classification/util.py
+++ classification/util.py
@@ -1,11 +1,12 @@
 from sklearn.model_selection import GridSearchCV
 
-def print_score(name, classifier, dataset):
+def get_trained_classifier(name, classifier, dataset):
     classifier.fit(dataset[0], dataset[2])
     score = classifier.score(dataset[1], dataset[3])
     print("{} score: {:.2f}%".format(name, score*100))
+    return classifier
 
-def print_tuned_score(parameters, classifier, dataset):
+def get_grid_search(parameters, classifier, dataset):
     grid_search = GridSearchCV(estimator=classifier,
                                param_grid=parameters,
                                scoring='accuracy',
@@ -14,3 +15,4 @@ def print_tuned_score(parameters, classifier, dataset):
     classifier = grid_search.best_estimator_
     score = classifier.score(dataset[1], dataset[3])
     print("Tuned parameters score: {:.2f}%".format(score * 100))
+    return grid_search
