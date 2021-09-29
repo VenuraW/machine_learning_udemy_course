d384a611f44f09dac723c134745f999abb3a9cf3
Venura <valw0002@student.monash.edu>
2021-09-29 14:09:39 +1000

    Refactoring for more modular code

--- classification/logistic_regression.py
+++ classification/logistic_regression.py
@@ -1,26 +1,13 @@
 # Importing the libraries
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.impute import SimpleImputer
-
-# Import dataset
-dataset = pd.read_csv("breast-cancer-wisconsin.data", header=None, na_values='?')
-x = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
-
-# Fill missing values
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-x = imputer.fit_transform(x)
-
-# Splitting the data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+import util
+from preprocessing import split_set
 
 # Training the model
-log_reg_classifier = LogisticRegression()
-log_reg_classifier.fit(x_train, y_train)
-log_reg_score = log_reg_classifier.score(x_test, y_test)
-print("Score: {:.2f}".format(log_reg_score*100))
+log_reg_classifier = util.get_trained_classifier("Logistic Regression", LogisticRegression(), split_set)
 
-
+# Hyperparamter Tuning
+parameters = [{'penalty': ['l2', 'none'], 'solver':['newton-cg', 'lbfgs']},
+              {'penalty': ['l1', 'l2'], 'solver': ['liblinear']},
+              {'penalty': ['l2'], 'solver': ['lbfgs']}]
+grid_search = util.get_grid_search(parameters, LogisticRegression(), split_set)
