84e9e65ada9fc786f9d8a0dacc1cca5a610fa488
Venura <valw0002@student.monash.edu>
2021-10-01 08:54:24 +1000

    Adding Feature Scaling

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
 # Import dataset
 dataset = pd.read_csv("breast-cancer-wisconsin.data", header=None, na_values='?')
@@ -13,4 +14,11 @@ imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 x = imputer.fit_transform(x)
 
 # Splitting the data
-split_set = train_test_split(x, y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
+
+# Feature Scaling
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+
